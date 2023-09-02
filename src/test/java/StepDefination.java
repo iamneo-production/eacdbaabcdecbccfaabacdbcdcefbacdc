5decb8bf285fe615053a9657a5d7df88f1108d64
Iamneo <techadmin@iamneo.ai>
2023-09-02 06:58:33 +0000

    Backup Commit

---
+++ src/test/java/StepDefination.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefination {
+    WebDriver driver=null;
+
+    @Given("^User is on registration page$")
+    public void goToRegistration(){
+        
+        driver.navigate().to("https://flipkart.com");
+    }
+
+    @Given("^User navigates to Login page$")
+    public void goToLoginPage(){
+        driver.findElement(By.linkText("login")).click();
+    }
+    @Given("^User enters username and password")
+    public void fillDetails(){
+        driver.findElement(By.id("Username")).sendKeys("ABC@!23");
+        driver.findElement(By.id("password")).sendKeys("qwerty@123");
+    }
+    @Then("^Login success or not")
+    public void verifyLogin(){
+        String expTitle = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
+        String orgTitle = driver.getTitle();
+        if(expTitle.equals(orgTitle)){
+            System.out.println("You have successfully logged into your account! There are multiple discount offers waiting for you!!");
+        }
+        else{
+            System.out.println("Login failed!!");
+        }
+        driver.close();
+    }
+}
